b8c45172b69ca03ea4ec8a8103f89e322e1e9bc1
Radif <23radif@gmail.com>
2020-08-22 21:50:07 +0300

    lesson 2   - base.html   - block   - include tag   - context   - template filter   - static template tag   - url template tag   - content in templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -23,7 +23,29 @@ def showroom(request):
 
 
 def contact(request):
+    locations = [
+        # from json, from pickle, from db
+        {
+            'city': 'California',
+            'phone': '1900 - 1234 - 5678',
+            'email': 'info@interior.com',
+            'address': '12 W 1st St, 90001 Los Angeles, California',
+        },
+        {
+            'city': 'Texas',
+            'phone': '1926 - 2314 - 3245',
+            'email': 'texas@interior.com',
+            'address': '24 W 1st St, 23554 Dallas County, Texas',
+        },
+        {
+            'city': 'Georgia',
+            'phone': '1999 - 2334 - 3457',
+            'email': 'georgia@interior.com',
+            'address': '23 W 1st St, 90001 Mogul, Georgia',
+        },
+    ]
     context = {
         'page_title': 'Contact',
+        'locations': locations,
     }
     return render(request, 'mainapp/contact.html', context)
