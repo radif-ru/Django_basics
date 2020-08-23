9f2464653c89e304e6a2be80bdb116fb0e101c67
Radif <23radif@gmail.com>
2020-08-23 13:38:30 +0300

    lesson 2   - base.html   - block   - include tag   - context   - template filter   - static template tag   - url template tag   - content in templates   - json load in contact.html   - links_menu.json   - add active link in menu

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 import json
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+with open(os.path.join(BASE_DIR, 'mainapp/templates/mainapp/jsons/links_menu.json')) as f:
+    links_menu = json.loads(f.read())
+
 
 def index(request):
     context = {
         'page_title': 'INTERIOR',
+        'links_menu': links_menu,
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -13,7 +19,8 @@ def index(request):
 def products(request):
     context = {
         'page_title': 'Products',
-        'exclusive_margin': ' exclusive_margin',
+        'add_class_names': ' exclusive_margin',
+        'links_menu': links_menu,
     }
     return render(request, 'mainapp/products.html', context)
 
@@ -21,18 +28,19 @@ def products(request):
 def showroom(request):
     context = {
         'page_title': 'Showroom',
+        'links_menu': links_menu,
     }
     return render(request, 'mainapp/showroom.html', context)
 
 
 def contact(request):
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    with open(os.path.join(base_dir, 'mainapp/templates/mainapp/json/contact_locations.json')) as f:
-        loc_json = f.read()
+    with open(
+            os.path.join(BASE_DIR, 'mainapp/templates/mainapp/jsons/contact_locations.json')) as f:
+        locations = json.loads(f.read())
 
-    locations = json.loads(loc_json)
     context = {
         'page_title': 'Contact',
         'locations': locations,
+        'links_menu': links_menu,
     }
     return render(request, 'mainapp/contact.html', context)
