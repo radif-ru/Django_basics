ea6e5d45b50f9dfe9d07655c8742be8b5fa6ec01
Radif <23radif@gmail.com>
2020-08-23 11:30:25 +0300

    lesson 2   - base.html   - block   - include tag   - context   - template filter   - static template tag   - url template tag   - content in templates   - json load in contact.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+import os
 
 
 def index(request):
@@ -11,6 +13,7 @@ def index(request):
 def products(request):
     context = {
         'page_title': 'Products',
+        'exclusive_margin': ' exclusive_margin',
     }
     return render(request, 'mainapp/products.html', context)
 
@@ -23,27 +26,11 @@ def showroom(request):
 
 
 def contact(request):
-    locations = [
-        # from json, from pickle, from db
-        {
-            'city': 'California',
-            'phone': '1900 - 1234 - 5678',
-            'email': 'info@interior.com',
-            'address': '12 W 1st St, 90001 Los Angeles, California',
-        },
-        {
-            'city': 'Texas',
-            'phone': '1926 - 2314 - 3245',
-            'email': 'texas@interior.com',
-            'address': '24 W 1st St, 23554 Dallas County, Texas',
-        },
-        {
-            'city': 'Georgia',
-            'phone': '1999 - 2334 - 3457',
-            'email': 'georgia@interior.com',
-            'address': '23 W 1st St, 90001 Mogul, Georgia',
-        },
-    ]
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    with open(os.path.join(base_dir, 'mainapp/templates/mainapp/json/contact_locations.json')) as f:
+        loc_json = f.read()
+
+    locations = json.loads(loc_json)
     context = {
         'page_title': 'Contact',
         'locations': locations,
