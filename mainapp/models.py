5b2fe8c40c8f9b2a9c42365bdbd9be57a9545dfc
Radif <23radif@gmail.com>
2020-08-26 23:09:47 +0300

    lesson 3   - import BASE_DIR   - as, |add: filter for inc_exclusive_products.html   - namespaces in urls   - 2 models

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,20 @@ class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя категории', max_length=128)
     description = models.TextField(verbose_name='описание категории', blank=True)
 
+    def __str__(self):
+        string = ''
+        if self.id:
+            string += f'\nid = {self.id}'
+        if self.name:
+            string += f'\nname = {self.name}'
+        if self.description:
+            string += f'\ndescription = {self.description}'
+        return string
+
+    class Meta:
+        verbose_name = 'Категория продукта'
+        verbose_name_plural = 'Категории продуктов'
+
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory,
@@ -16,3 +30,27 @@ class Product(models.Model):
     description = models.TextField('описание продукта', blank=True)
     price = models.DecimalField('цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField('количество на складе', default=0)
+
+    def __str__(self):
+        string = ''
+        if self.id:
+            string += f'\nid = {self.id}'
+        if self.name:
+            string += f'\nname = {self.name}'
+        if self.short_desc:
+            string += f'\nshort_desc = {self.short_desc}'
+        if self.description:
+            string += f'\ndescription = {self.description}'
+        if self.price:
+            string += f'\nprice = {self.price}'
+        if self.quantity:
+            string += f'\nquantity = {self.quantity}'
+        if self.category:
+            string += f'\ncategory_id = {self.category_id}'
+            string += f'\n\n*ProductCategory.name = {self.category.name}'
+            string += f'\n*ProductCategory.description = {self.category.description}'
+        return string
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
